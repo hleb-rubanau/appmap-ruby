58065262754e4cbf616230ed849bf2b4d0e02e33
Hleb Rubanau <g.rubanau@gmail.com>
2021-04-15 19:15:07 +0300

    fix(build): stupidiest typo

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 # using bash wrapper as Rake blows up in `require/extentiontask` (line 10)
 
 RELEASE_FLAGS=""
-if [ -e "$TRAVIS_REPO_SLUG" ]; then
+if [ ! -z "$TRAVIS_REPO_SLUG" ]; then
     RELEASE_FLAGS="-r git+https://github.com/${TRAVIS_REPO_SLUG}.git"
 fi 
 
