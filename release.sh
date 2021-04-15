5ba4d9c4f163741e5c22449c23856ee2de21e29f
Hleb Rubanau <g.rubanau@gmail.com>
2021-04-15 18:40:02 +0300

    fix typo

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 # using bash wrapper as Rake blows up in `require/extentiontask` (line 10)
 
 RELEASE_FLAGS=""
 if [ -e "$TRAVIS_REPO_SLUG" ]; then
-    RELEASE_FLAGS ="-r git+https://github.com/${TRAVIS_REPO_SLUG}.git"
+    RELEASE_FLAGS="-r git+https://github.com/${TRAVIS_REPO_SLUG}.git"
 fi 
 
+set -x
 exec semantic-release $RELEASE_FLAGS
 
